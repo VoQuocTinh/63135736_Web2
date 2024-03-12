468f14836f319390dd9fbe9a40eb62dc63ab4520
VoQuocTinh <133622416+VoQuocTinh@users.noreply.github.com>
2024-03-12 17:10:51 +0700

    juh

---
+++ HelloWorldSpringBoot-2/src/main/java/com/example/HelloWorld/HelloWorldSpringBoot2Application.java
@@ -0,0 +1,13 @@
+package com.example.HelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringBoot2Application.class, args);
+	}
+
+}
