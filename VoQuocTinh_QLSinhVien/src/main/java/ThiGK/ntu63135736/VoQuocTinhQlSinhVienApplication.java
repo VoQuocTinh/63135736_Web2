17a8e69ec6fcb088f1ae901d38b092cb9703746d
VoQuocTinh <133622416+VoQuocTinh@users.noreply.github.com>
2024-03-26 15:11:32 +0700

    push lan 1

---
+++ VoQuocTinh_QLSinhVien/src/main/java/ThiGK/ntu63135736/VoQuocTinhQlSinhVienApplication.java
@@ -0,0 +1,13 @@
+package ThiGK.ntu63135736;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoQuocTinhQlSinhVienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoQuocTinhQlSinhVienApplication.class, args);
+	}
+
+}
