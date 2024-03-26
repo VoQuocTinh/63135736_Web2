81a3d1072c38b2237daf0a63340bca725ae7d15c
VoQuocTinh <133622416+VoQuocTinh@users.noreply.github.com>
2024-03-26 15:44:15 +0700

    push lan 2

---
+++ VoQuocTinh_QLSinhVien/src/main/java/ThiGK/ntu63135736/controller/Maincontroller.java
@@ -0,0 +1,10 @@
+package ThiGK.ntu63135736.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class Maincontroller {
+	  @GetMapping({"/", "/home"})
+	    public String index() {
+	        return "index";
+	    }
+}
