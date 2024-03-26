15b008491bdfb75faf248c74c4bea695ff496809
VoQuocTinh <133622416+VoQuocTinh@users.noreply.github.com>
2024-03-26 16:59:35 +0700

    push lan 5

--- VoQuocTinh_QLSinhVien/src/main/java/ThiGK/ntu63135736/controller/StudentController.java
+++ VoQuocTinh_QLSinhVien/src/main/java/ThiGK/ntu63135736/controller/StudentController.java
@@ -1,41 +1,32 @@
 package ThiGK.ntu63135736.controller;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import ThiGK.ntu63135736.models.Student;
 import ThiGK.ntu63135736.service.Studentservice;
 
 @RestController
-@RequestMapping("/students")
 public class StudentController {
 
     @Autowired
     private Studentservice studentService;
 
-    // Endpoint để lấy toàn bộ danh sách sinh viên
-    @RequestMapping("/")
+    @GetMapping("/students")
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
     }
 
-    // Endpoint để tìm kiếm sinh viên theo mã sinh viên
-    @RequestMapping("/id")
-    public Student getStudentById(@PathVariable("id") String id) {
+    @GetMapping("/students/{id}")
+    public Student getStudentById(@PathVariable String id) {
         return studentService.findStudentById(id);
     }
 
-    // Endpoint để thêm mới một sinh viên
-    @PostMapping("/")
-    public ResponseEntity<Void> addStudent(@RequestBody Student student) {
+    @PostMapping("/students")
+    public void addStudent(@RequestBody Student student) {
         studentService.addStudent(student);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
