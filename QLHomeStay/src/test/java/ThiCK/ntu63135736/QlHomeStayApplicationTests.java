61c8cd49160dceb3863d6d115ff81c02fe9a0f57
VoQuocTinh <133622416+VoQuocTinh@users.noreply.github.com>
2024-04-16 15:32:30 +0700

    new

---
+++ QLHomeStay/src/test/java/ThiCK/ntu63135736/QlHomeStayApplicationTests.java
@@ -0,0 +1,13 @@
+package ThiCK.ntu63135736;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlHomeStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
