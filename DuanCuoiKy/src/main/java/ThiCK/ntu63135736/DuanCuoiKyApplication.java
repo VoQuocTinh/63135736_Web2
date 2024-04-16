732a8639b8fc4d5e9fdec913babe058a00fa5eb5
VoQuocTinh <133622416+VoQuocTinh@users.noreply.github.com>
2024-04-16 15:37:05 +0700

    new

---
+++ DuanCuoiKy/src/main/java/ThiCK/ntu63135736/DuanCuoiKyApplication.java
@@ -0,0 +1,13 @@
+package ThiCK.ntu63135736;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuanCuoiKyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuanCuoiKyApplication.class, args);
+	}
+
+}
---
+++ DuanCuoiKy/src/test/java/ThiCK/ntu63135736/DuanCuoiKyApplicationTests.java
@@ -0,0 +1,13 @@
+package ThiCK.ntu63135736;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuanCuoiKyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
